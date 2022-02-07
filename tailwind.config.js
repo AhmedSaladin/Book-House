74f3ece0223a0731f8cd41d8ac44a62451eb61ee
Ahmed Saladin <as.yuri74@gmail.com>
2022-02-08 00:55:08 +0200

    Finish book house tasks

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mc: "#3F3D56",
+        nc: "#E5E5E5",
+        nb: "#5D5D5D",
+        bb: "#2F2E41",
+        mcm: "#464346",
+        ce:"#EEEEEE"
+      },
+      fontFamily: {
+        body: ["Roboto"],
+      },
+    },
+  },
+  plugins: [],
+};
