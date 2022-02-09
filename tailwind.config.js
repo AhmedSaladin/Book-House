7b7c5631cd86bc3bd948fd9ed39f4055d99b54d1
Ahmed Saladin <as.yuri74@gmail.com>
2022-02-09 03:29:28 +0200

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,21 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       colors: {
         mc: "#3F3D56",
         nc: "#E5E5E5",
         nb: "#5D5D5D",
         bb: "#2F2E41",
         mcm: "#464346",
-        ce:"#EEEEEE"
+        ce: "#EEEEEE",
+        fb: "url('assets/bg.png')",
       },
       fontFamily: {
         body: ["Roboto"],
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
