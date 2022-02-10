4a933b3fb35759cf1b37838ff377862bfc7c0f4b
Ahmed Saladin <as.yuri74@gmail.com>
2022-02-10 16:12:37 +0200

    Add All files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,53 @@
 module.exports = {
-  content: ["./*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      container: {
-        center: true,
-      },
-      colors: {
-        mc: "#3F3D56",
-        nc: "#E5E5E5",
-        nb: "#5D5D5D",
-        bb: "#2F2E41",
-        mcm: "#464346",
-        ce: "#EEEEEE",
-        fb: "url('assets/bg.png')",
-      },
-      fontFamily: {
-        Roboto: ["Roboto"],
-        sans: ["Poppins", "sans-serif"],
-      },
+    container: {
+      center: true,
+      padding: "4rem",
     },
+    colors: {
+      purple: "#3F3D56",
+      lp: "#5d5b7a",
+      grey: "#464346",
+      white: "#ffffff",
+      lightGrey: "#5D5D5D",
+      black: "#000",
+      mc: "#3F3D56",
+      nc: "#E5E5E5",
+      nb: "#5D5D5D",
+      bb: "#2F2E41",
+      mcm: "#464346",
+      ce: "#EEEEEE",
+      fb: "url('assets/bg.png')",
+    },
+    backgroundImage: {
+      "hero-pattern": "url('../src/images/bg.png')",
+    },
+    fontFamily: {
+      Roboto: ["Roboto"],
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "640px",
+          },
+          "@screen md": {
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            maxWidth: "1280px",
+          },
+          "@screen xl": {
+            maxWidth: "1280px",
+          },
+        },
+      });
+    },
+  ],
 };
